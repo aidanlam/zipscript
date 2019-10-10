d970cf083d4cc2579048efacea4ab8e2c1e13eff
Aidan Lam <32441938+aidanlam@users.noreply.github.com>
2019-10-09 17:48:13 -0700

    Update zipscript.ps1

--- zipscript.ps1
+++ zipscript.ps1
@@ -2,8 +2,10 @@ $password = ""
 
 $files = gci . *.* -rec | where { ! $_.PSIsContainer }
 set-alias 7z "$env:ProgramFiles\7-Zip\7z.exe"
+
 foreach ($file in $files) {
-    $path = Get-Item -Path $file.PSPath #EXAMPLE: C:\batch\test_dir_2\test_dir_2_file_1.txt
-    sz a -t7z -p$password "$path.7z" "$path" 
+    $path = Get-Item -Path $file.PSPath
+    echo "Archiving path: $path | file: $file | password: $password"
+    7z a -t7z "$path.7z" "$path" -p"$password"
     rm $path
 }
